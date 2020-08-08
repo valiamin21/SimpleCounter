d6d879423cdf670173c2d31e12aa614dd368c96f
Amin <valiamin21@gmail.com>
2020-08-08 11:52:15 +0430

    adding toolbar

--- app/src/main/java/ir/proglovving/simplecounter/MainActivity.java
+++ app/src/main/java/ir/proglovving/simplecounter/MainActivity.java
@@ -1,28 +1,30 @@
 package ir.proglovving.simplecounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import ir.proglovving.cfviews.CTypefaceProvider;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton increaseButton, decreaseButton;
     private TextView numTextView;
+    Toolbar toolbar;
 
-    private int x = 0;
+    private int x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        x = CounterPrefManager.getNumber(this);
 
-        numTextView = findViewById(R.id.num_tv);
-        setNumber(CounterPrefManager.getNumber(this));
-
-        ImageButton increaseButton = findViewById(R.id.increase_btn);
-        ImageButton decreaseButton = findViewById(R.id.decrease_btn);
+        initViews();
 
         increaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,19 +41,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setNumber(int x){
+    private void initViews() {
+        numTextView = findViewById(R.id.num_tv);
+        setNumber(CounterPrefManager.getNumber(this));
+
+        increaseButton = findViewById(R.id.increase_btn);
+        decreaseButton = findViewById(R.id.decrease_btn);
+
+        toolbar = findViewById(R.id.toolbar);
+        for (int i = 0; i < toolbar.getChildCount(); i++) {
+            if (toolbar.getChildAt(i) instanceof TextView) {
+                ((TextView) toolbar.getChildAt(i)).setTypeface(CTypefaceProvider.getVazirBold(this));
+            }
+        }
+    }
+
+    private void setNumber(int x) {
         numTextView.setText(String.valueOf(x));
     }
 
-    private void increaseNumber(){
+    private void increaseNumber() {
         x++;
         setNumber(x);
-        CounterPrefManager.saveNumber(this,x);
+        CounterPrefManager.saveNumber(this, x);
     }
 
-    private void decreaseNumber(){
+    private void decreaseNumber() {
         x--;
         setNumber(x);
-        CounterPrefManager.saveNumber(this,x);
+        CounterPrefManager.saveNumber(this, x);
     }
 }
